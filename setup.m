f3523d2a8b6fa17ce1c2fcdbb3cfefabbcf4c1c1
frankpermenter <fperment@mit.edu>
2013-06-29 07:02:34 -0400

    Made setup a function call

--- setup.m
+++ setup.m
@@ -1,20 +1,21 @@
-%Checking dependencies
-sedumiExists = ~isempty(which('sedumi'));
-gurobiExists = ~isempty(which('gurobi'));
+function setup()
+    %Checking dependencies
+    sedumiExists = ~isempty(which('sedumi'));
+    gurobiExists = ~isempty(which('gurobi'));
 
-display('Checking dependencies...')
-if ~sedumiExists
-    error('Cannot find sedumi. Aborting...')
-end
+    display('Checking dependencies...')
+    if ~sedumiExists
+        error('Cannot find sedumi. Aborting...')
+    end
 
-if ~gurobiExists
-    error('Cannot find gurobi. Aborting...')
-end
+    if ~gurobiExists
+        error('Cannot find gurobi. Aborting...')
+    end
 
-display('Updating path...')
-addpath([pwd,'/util'])
-addpath([pwd,'/test'])
-addpath(pwd)
+    display('Updating path...')
+    addpath([pwd,'/util'])
+    addpath([pwd,'/test'])
+    addpath(pwd)
 
-display('Done!')
-display('Type runTests to check installation')
+    display('Done!')
+    display('Type runTests to check installation')
