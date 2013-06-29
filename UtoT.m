d928f691e92fd0d6b70d876641901ceebd507934
frankpermenter <fperment@mit.edu>
2013-06-29 06:37:19 -0400

    Added fuction for compuating matrix T for which Tq(:) = U q U^T (:)

---
+++ UtoT.m
@@ -0,0 +1,26 @@
+
+function T =  UtoT(U)
+    [N,M]=size(U);
+
+    %linear map of q(:) to (U*q)(:)
+    T1 = U;
+    for i=2:M
+        T1=blkdiag(T1,U); 
+    end
+
+    %linear map of (U*q)(:) to (U*q*U')(:)
+    %for symmetric q
+    Ut = U';
+    T2 = sparse([]);
+    for i=1:N
+        indx = sub2ind([N,M],i*ones(1,M),1:M);
+        for j=1:N
+            T2(j+(i-1)*N,indx) = Ut(:,j);
+        end
+    end
+
+    T = T2*T1;
+    %Q1 = U*q*U';
+    %Q2 = mat(T2*T1*q(:));
+
+end
