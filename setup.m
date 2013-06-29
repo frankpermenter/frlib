cbbe4ec356f4288cf8a50a08bd4a1c7beecbd704
frankpermenter <fperment@mit.edu>
2013-06-29 07:00:14 -0400

    Added setup.

---
+++ setup.m
@@ -0,0 +1,20 @@
+%Checking dependencies
+sedumiExists = ~isempty(which('sedumi'));
+gurobiExists = ~isempty(which('gurobi'));
+
+display('Checking dependencies...')
+if ~sedumiExists
+    error('Cannot find sedumi. Aborting...')
+end
+
+if ~gurobiExists
+    error('Cannot find gurobi. Aborting...')
+end
+
+display('Updating path...')
+addpath([pwd,'/util'])
+addpath([pwd,'/test'])
+addpath(pwd)
+
+display('Done!')
+display('Type runTests to check installation')
