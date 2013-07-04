b69e42da1a2c937d0f5248869f02060c95041f51
frankpermenter <fperment@mit.edu>
2013-07-04 13:06:51 -0400

    Added stronger pass criteria

--- test/runTests.m
+++ test/runTests.m
@@ -3,13 +3,28 @@ clear all;
 load hybridLyap.mat;
 A = A';
 Z = frlibPrg(A,b,c,K);
+
+%diagonal
+prg = Z.ReducePrimal('d');
+[x,y] = prg.Solve();
+xO = prg.RecoverPrimal(x);
+pass  = prg.CheckPrimal(x);
+pass = pass & all( prg.K.s ==[6 56 11 1 1 0 11 1 1 0 11 11]);
+if ~(pass)
+    error('Test case failed')
+end
+
+%diagonally dominant
 prg = Z.ReducePrimal('dd');
 [x,y] = prg.Solve();
 xO = prg.RecoverPrimal(x);
 pass  = prg.CheckPrimal(x);
+pass = pass & all( prg.K.s == [6 34 8 1 1 0 8 1 1 0 9 7]);
 if ~(pass)
     error('Test case failed')
 end
+prg
+break;
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 load testDual.mat;
 
