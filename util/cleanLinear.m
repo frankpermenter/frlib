5e96bd682f9db545077d8d09cfdd23c3141809e9
frankpermenter <frankpermenter@gmail.com>
2013-07-04 11:52:34 -0500

    Use sparse qr.

--- util/cleanLinear.m
+++ util/cleanLinear.m
@@ -1,15 +1,22 @@
 function [A,b,T]= cleanLinear(A,b)
     
-    [~,R,P]=qr([A,b]',0);
-    eqKeep = P( find(abs(diag(R)) > eps));
+    R = qr(sparse([A,b]'));
+	[r,c] = find(R);
+	%the first non-zero entry on a row implies
+	%the column (e.g. equation) is linearly independent
+	[~,indx] = unique(r,'first');	
+	eqKeep = c(indx);
+	
+	%must be non-zero and must be first col
     eqRmv = setdiff(1:size(A,1),eqKeep);
-    eqKeep = sort(eqKeep);
-
+	
     T = speye(size(A,1));
+	
     T(eqRmv,:) = 0;
     T = T(:,eqKeep);
 
     A = A(eqKeep,:);
     b = b(eqKeep,:);
-
+	
+	
 
