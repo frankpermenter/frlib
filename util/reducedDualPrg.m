178d21356d87130115278e3d76dab31d466202eb
frankpermenter <fperment@mit.edu>
2014-05-28 15:16:25 -0400

    Removed dual recovery code

--- util/reducedDualPrg.m
+++ util/reducedDualPrg.m
@@ -1,6 +1,6 @@
 classdef reducedDualPrg < frlibPrg
 
-    properties
+    properties (GetAccess=protected)
         Uarry
         Varry
         Sarry
@@ -23,8 +23,7 @@ classdef reducedDualPrg < frlibPrg
                 Tuu = cone.BuildMultMap(U{end},U{end});
                 Tuv = cone.BuildMultMap(U{end},V{end});
                 Tvv = cone.BuildMultMap(V{end},V{end});
-
-                
+           
                 [s,e] = cone.flqrIndx();
                 Tvv(s:e,:) = 0;
                 Tuv(s:e,:) = 0;
@@ -34,7 +33,6 @@ classdef reducedDualPrg < frlibPrg
            
                 DualEqA = [DualEq1,DualEq2];
                 DualEqC = [(c(:)'*Tvv'),c(:)'*Tuv'];
-
 
                 solMap.shat_vvt.s = 1;
                 solMap.shat_vvt.e = solMap.shat_vvt.s + size(DualEq1,2)-1;
@@ -46,6 +44,7 @@ classdef reducedDualPrg < frlibPrg
                 Kr = ConeBase.cleanK(Karry{end});
                 Kr.f = Kr.f + size(DualEqA,2);
                 noReductions = 0;
+
             else
                
                 Kr = K;
@@ -53,6 +52,7 @@ classdef reducedDualPrg < frlibPrg
                 cr = c;
                 solMap ={};
                 noReductions = 1;
+
             end
 
             
@@ -61,9 +61,7 @@ classdef reducedDualPrg < frlibPrg
             self.Sarry = S;
             self.Karry = Karry;
             self.Varry = V;
-            self.Aorig = A;
-            self.corig = c;
-            self.Korig = K;
+
             self.solMap = solMap;
             self.noReductions = noReductions;
 
@@ -72,153 +70,6 @@ classdef reducedDualPrg < frlibPrg
 
         end
 
-        function [xr,x0] = RecoverRamana(self,x,eps)
-            
-            if (self.noReductions)
-                xr = x; x0 = x; 
-                return
-            end
-            
-            
-            if ~exist('eps','var')
-                eps = 10^-4;
-            end
-            
-            U = self.Uarry; V = self.Varry;
-            solMap = self.solMap;
-            coneOrig = ConeBase(self.Korig);
-            
-            s = self.cone.GetIndx('s',1);
-            sBar = x(s:end);
-            sHat = x(solMap.shat_vvt.s:solMap.shat_vvt.e);
-            beta = x(solMap.beta_uvt.s:solMap.beta_uvt.e);
-            
-            
-            if ~all(cellfun(@isempty,U))     
-                [x,x11,x22,x21] = coneOrig.ConjBlock2by2(sBar,sHat,beta/2,U{end},V{end});
-            else
-                x = sBar;
-            end          
-            
-            
-            
-            V = self.Varry{end}{1};
-            U = self.Uarry{end}{1};       
-            
-            x = mat(x);
-            
-            xpsd = U*U'*x * U*U';
-            xperp = x-xpsd;
-            
-            
-            Gpsd{1} = mat(self.Sarry{1});
-            Gperp{1} = zeros( size(Gpsd{1},1));
-            for i=2:length(self.Sarry)
-                G = 0;
-                
-               
-                G =  mat(self.Sarry{i});   
-             
-                
-                
-                V = self.Varry{i-1}{1};
-                U = self.Uarry{i-1}{1};
-                
-                
-                
-                Gpsd{i} =  Gpsd{i-1} + U * U' * G  * U * U';
-                Gperp{i} = Gperp{i-1} + G-U * U' * G  * U * U';
-                
-                if (trace(Gperp{i}*U*U'))
-                  error('projection failed')
-                end
-
-                
-            end
-            
-            
-            
-            full([Gpsd{end},xperp;xperp',eye(4)])
-            
-  
-            
-            
-        end
-        
-        
-        
-        function [xr,x0] = RecoverPrimal(self,x,eps)
-
-            if (self.noReductions)
-                xr = x; x0 = x; 
-                return
-            end
-            
-            
-            if ~exist('eps','var')
-                eps = 10^-4;
-            end
-            
-            U = self.Uarry; V = self.Varry;
-            solMap = self.solMap;
-            coneOrig = ConeBase(self.Korig);
-            
-            s = self.cone.GetIndx('s',1);
-            sBar = x(s:end);
-            sHat = x(solMap.shat_vvt.s:solMap.shat_vvt.e);
-            beta = x(solMap.beta_uvt.s:solMap.beta_uvt.e);
-            
-            
-            xflqr = coneOrig.GetIndx('s',1);
-            xflqr = x(solMap.beta_uvt.e+1:s-1);
-            
-            if ~all(cellfun(@isempty,U))     
-                [x,x11,x22,x21] = coneOrig.ConjBlock2by2(sBar,sHat,beta/2,U{end},V{end});
-            else
-                x = sBar;
-            end
-
-            x(1:length(xflqr)) = xflqr;
-            xr = x; 
-            x0 = xr;
-            fail = [];
-            
-            for i=length(U):-1:1
-
-                fail(i) = 1;
-                if (i >= 2)
-
-                    Uface = U{i-1};
-
-                else
-
-                    Uface = [];
-
-                end
-
-               delta = 0.1;
-
-               for k = 1:100
-                    xr = x+self.Sarry{i}(:)'*delta;
-                    feas = SolUtil.CheckInFace(xr,Uface,self.Korig,eps);
-                    if (feas == 0)
-                        delta = delta+1;
-                    else
-                        x = xr;
-                        fail(i) =  0;
-                        break;
-                    end
-                end
-
-
-
-            end
-                      
-                if (any(fail))
-                   error('failed') 
-                end
-        
-        end
 
     end
 
