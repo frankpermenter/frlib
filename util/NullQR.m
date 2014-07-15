5627d9bea51066b0466276d7a58d8b1ee5aa3459
frankpermenter <fperment@mit.edu>
2014-07-15 13:16:13 -0400

    Added eps threshold argument.  Treat inputs that are vectors in a special way

--- util/NullQR.m
+++ util/NullQR.m
@@ -1,11 +1,20 @@
-function [nullA,rangeAt] = NullQR(A)
+function [nullA,rangeAt] = NullQR(A,eps)
+
+    if ~exist('eps','var')
+        eps = 10^-8;
+    end
 
     n = size(A,1);
     [q,r,e] = qr(A');
     [e,~] = find(e);
 
     %find vectors not in span of A'
-    p = find(abs(diag(r)) < 10^-8);
+    if size(r,2) == 1 || size(r,1) == 1
+        p = find(abs(r(1,1)) < eps);
+    else
+        p = find(abs(diag(r)) < eps);
+    end
+    
     if ~isempty(p)
         indxNull = min(p):size(q,2);
     else
